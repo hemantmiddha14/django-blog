99f461710712469ca7e77bff8bbda38591d0e2af
Hemant <hemantmiddha1412@gmail.com>
2018-06-21 19:17:24 +0530

    account

--- account/views.py
+++ account/views.py
@@ -3,29 +3,17 @@ from django.contib.auth import authenticate, login
 from .forms import UserForm
 
 
-
-class UserFormView(View):
-    form_class = UserForm
+def register(request):
     template_name = 'account/registeration_form.html'
-
-    def get(self, request):
-        form = self.form_class(None)
-        return render(request, self.template_name{'form': form})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-
-        if form.is_valid():
-            user = form.save(commit=False)
-
-            username = form.cleaned_Data['username']
-            password = form.cleaned_Data['password']
-            user.set_password(password)
-            user.save()
-
-            user = authenticate(username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect('/')
-        return render(request, self.template_name{'form': form})
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+    return render(request, self.template_name{'form': form})
